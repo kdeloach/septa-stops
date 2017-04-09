2b889a1e9320147b54643cf60da614596fe7707b
Kevin DeLoach <kdeloach@gmail.com>
2017-04-09 14:22:40 -0400

    Use union of all trips
    Generate route trace by creating a Polyline for all possible trips the
    unioning them together to form a MultiLineString. The purpose of this is
    to produce a more accurate route trace. In most cases, the results are
    much better than just relying on non-contiguous distinct stops. However,
    there are some routes where unioning all possible trips is more
    confusing.

--- query.sql
+++ query.sql
@@ -1,56 +1,51 @@
 with
 
--- Return distinct stop times. Most trips contain the same stops so this
--- filters out a lot of noise. The stop sequence order is not guaranteed
--- to be contiguous.
+-- Return stops from all trips ordered by stop sequence.
 -- Ex.
--- 45 | 0 | 1234 | 1
--- 45 | 0 | 1235 | 2
--- 45 | 0 | 1236 | 2
--- 45 | 0 | 1237 | 3
-distinct_stop_times as (
-    select distinct * from (
-        select
-            t.route_id,
-            t.direction_id,
-            st.stop_id,
-            st.stop_sequence
-        from stop_times st
-        inner join trips t on t.trip_id = st.trip_id
-    )
-),
-
--- Return distinct stops from all route trips ordered
--- by direction and stop sequence.
--- Ex.
--- 45 | 0 | 1 | <Point>
--- 45 | 0 | 2 | <Point>
--- 45 | 1 | 1 | <Point>
--- 45 | 1 | 2 | <Point>
+-- 45 | Trip 1 | 0 | 1 | <Point>
+-- 45 | Trip 2 | 0 | 2 | <Point>
+-- 45 | Trip 3 | 1 | 1 | <Point>
+-- 45 | Trip 4 | 1 | 2 | <Point>
 stops_per_trip as (
     select
-        dst.route_id,
-        dst.direction_id,
-        dst.stop_sequence,
+        t.route_id,
+        t.trip_id,
+        t.direction_id,
+        st.stop_sequence,
         MakePoint(s.stop_lon, s.stop_lat) point
-    from distinct_stop_times dst
-    inner join stops s on s.stop_id = dst.stop_id
+    from stop_times st
+    inner join trips t on t.trip_id = st.trip_id
+    inner join stops s on s.stop_id = st.stop_id
     order by
-        dst.route_id,
-        dst.direction_id,
-        dst.stop_sequence
+        t.route_id,
+        t.trip_id,
+        st.stop_sequence
 ),
 
--- Return Polyline for each route and direction.
+-- Return Polyline for each trip.
 -- Ex.
--- 45 | 0 | <Polyline>
--- 45 | 1 | <Polyline>
+-- 45 | Trip 1 | 0 | <Polyline>
+-- 45 | Trip 2 | 1 | <Polyline>
 lines_per_trip as (
     select
         route_id,
+        trip_id,
         direction_id,
         MakeLine(point) line
     from stops_per_trip
+    group by trip_id
+),
+
+-- Return all route trip lines unioned together as one shape.
+-- Ex.
+-- 45 | 0 | <Geometry>
+-- 45 | 1 | <Geometry>
+lines_per_route as (
+    select
+        route_id,
+        direction_id,
+        GUnion(line) line
+    from lines_per_trip
     group by route_id, direction_id
 )
 
@@ -61,9 +56,9 @@ select
     r.route_short_name,
     AsGeoJSON(n.line) line0,
     AsGeoJSON(s.line) line1
-from lines_per_trip n
+from lines_per_route n
 -- Join against the opposite direction line
-inner join lines_per_trip s
+inner join lines_per_route s
     on s.route_id = n.route_id
     and s.direction_id != n.direction_id
 inner join routes r on r.route_id = n.route_id
